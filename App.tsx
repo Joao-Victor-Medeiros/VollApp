db27fc619a298cdcc4bd02ae7a963b091a34d7ee
JoaoVictorMedeiros <joaovictormedeiros1505@gmail.com>
2023-10-02 15:20:56 -0300

    changing to stack navigation the App flow

--- App.tsx
+++ App.tsx
@@ -1,16 +1,13 @@
 import {NativeBaseProvider, StatusBar} from "native-base";
 
 import {THEMES} from "./src/styles/themes";
-import SignUp from "./src/SignUp";
-import Login from "./src/Login";
-import {NavigationContainer} from "@react-navigation/native";
+import Routes from "./src/Routes";
 
 export default function App() {
     return (
         <NativeBaseProvider theme={THEMES}>
             <StatusBar backgroundColor={THEMES.colors.blue[800]}/>
-            {/*<Login/>*/}
-            <SignUp/>
+            <Routes />
         </NativeBaseProvider>
     );
 }
